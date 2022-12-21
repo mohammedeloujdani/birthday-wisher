40e323faf4b30ad32e6f8bac0d58481822d1255e
mohammedeloujdani <mohammed.eloujdani1@gmail.com>
2022-12-21 14:37:15 +0100

    Updates

---
+++ precache-manifest.faa9934c8659cef66b3c2fa8da295e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7c26eacb5d50d760c0e23c6b522efc",
+    "url": "/birthday-wisher/index.html"
+  },
+  {
+    "revision": "b4913213b592755803d6",
+    "url": "/birthday-wisher/static/css/main.26086a97.chunk.css"
+  },
+  {
+    "revision": "80c274199a213c528ef1",
+    "url": "/birthday-wisher/static/js/2.5dc2bac3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/birthday-wisher/static/js/2.5dc2bac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4913213b592755803d6",
+    "url": "/birthday-wisher/static/js/main.864c2a19.chunk.js"
+  },
+  {
+    "revision": "66f290ee618f9ca64d4e",
+    "url": "/birthday-wisher/static/js/runtime-main.f353cf11.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/birthday-wisher/static/media/githubLogo.90118e04.svg"
+  }
+]);
